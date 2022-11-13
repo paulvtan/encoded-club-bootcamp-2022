483dadec986273ac766e3c198bba0dd2503a3533
Paul Tanchareon <paul.vtan@live.com>
2022-11-13 23:27:53 +1300

    Learn about and Implemented basic unit test for HelloWorld.sol

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig, task } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
+import "@nomiclabs/hardhat-ethers"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
