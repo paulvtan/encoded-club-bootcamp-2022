9dbee7593e94b3ddd8713795a3419cbb14f80c0a
Paul Tanchareon <paul.vtan@live.com>
2022-11-16 16:52:47 +1300

    Updated delegate.ts script to support delegator and delegatee parameters.

--- scripts/week2/Delegate.ts
+++ scripts/week2/Delegate.ts
@@ -7,19 +7,21 @@ import { displayAccountInfo, getSigner } from "../common/Helper"
 
 dotenv.config()
 
-// This script delegate your vote to the target address.
-// example: yarn run ts-node --files .\scripts\week2\Delegate.ts "0xaAC3C56101cbb1D2Be1492599Fa8dbc515d12CB2" "0x40262E621D9250f0D04D03503145E8dB8515f796"
+// This script delegate a vote from one of your wallet account to a target address.
+// example: yarn run ts-node --files .\scripts\week2\Delegate.ts "0xE05986EFe30A0d48baaF10980c07f51eBAA8E603" "1" "0xB08A2ea5186b572F983cA783eFb8c4E6D2bde29e"
 // Reference: https://docs.soliditylang.org/en/v0.8.17/solidity-by-example.html
 async function main() {
   const contractAddress = process.argv[2]
-  const targetAddress = process.argv[3]
+  const delegatorAccNo = process.argv[3]
+  const delegateeAddress = process.argv[4]
+
   console.log(`Attaching to contract: ${contractAddress}`)
-  const signer = getSigner()
+  const signer = getSigner(Number(delegatorAccNo))
   await displayAccountInfo(signer)
   const ballotContractFactory = new Ballot__factory(signer)
   const ballotContract = ballotContractFactory.attach(contractAddress)
-  console.log(`Delegating voting right to target address: ${targetAddress}`)
-  const tx = await ballotContract.delegate(targetAddress)
+  console.log(`Delegating voting right to target address: ${delegateeAddress}`)
+  const tx = await ballotContract.delegate(delegateeAddress)
   await tx.wait()
   console.log(`Success - tx hash: ${tx.hash}`)
 }
