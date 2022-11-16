1aa3ddb8b218f1dba3f7fbf5dddb5742a93450d2
Paul Tanchareon <paul.vtan@live.com>
2022-11-16 15:54:31 +1300

    - getSigner() helper function to easily get desired account. - CheckAccountsBalance to quickly output each of the account balance of a wallet.

--- scripts/common/Helper.ts
+++ scripts/common/Helper.ts
@@ -1,7 +1,9 @@
 import { ethers } from "ethers"
+import * as dotenv from "dotenv"
+dotenv.config()
 
 /// Accept a signer and display connected account information.
-export async function displayAccountInfo(signer: ethers.Wallet) {
+export const displayAccountInfo = async (signer: ethers.Wallet) => {
   const balanceWei = await signer.getBalance()
   const balanceEth = ethers.utils.formatEther(balanceWei)
   console.log("\n")
@@ -17,4 +19,19 @@ export async function displayAccountInfo(signer: ethers.Wallet) {
   )
   console.log("\n")
   return balanceWei
+}
+
+// Returns a signer object with specified account number - i.e. 0 is default account of the wallet.
+export const getSigner = (accountNo: number = 0) => {
+  const provider = ethers.getDefaultProvider("goerli", {
+    alchemy: process.env.ALCHEMY_API_KEY,
+    etherscan: process.env.ETHERSCAN_API_KEY,
+    infura: process.env.INFURA_API_KEY,
+  })
+  const accountPath = `m/44'/60'/0'/0/${accountNo}`
+  const wallet = ethers.Wallet.fromMnemonic(
+    process.env.MNEMONIC ?? "",
+    accountPath
+  )
+  return wallet.connect(provider)
 }
