fa680eded1514cb85009ad5dfe5fafdc5187797c
Paul Tanchareon <paul.vtan@live.com>
2022-11-28 15:46:21 +1300

    Added various base project configurations for frontend project.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { ethers } from 'ethers'
+import { environment } from '../environments/environment'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'frontend'
+  lastBlockNumber: number | undefined
+
+  constructor() {
+    ethers.providers
+      .getDefaultProvider('goerli')
+      .getBlock('latest')
+      .then((block) => {
+        this.lastBlockNumber = block.number
+      })
+  }
 }
