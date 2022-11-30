8dcaa7b737c7d25f214d6f71cc5b33c04b501e8b
Paul Tanchareon <paul.vtan@live.com>
2022-11-30 17:05:40 +1300

    Added basic frontend implementation.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
-import { ethers } from 'ethers'
-import { environment } from '../environments/environment'
+import { BigNumber, ethers } from 'ethers'
+import VoteTokenJson from '../assets/VoteToken.json'
+
+const ERC20VOTES_TOKEN_ADDRESS = '0x111725C04643306563ed5f906aF0Ac6790898495'
 
 @Component({
   selector: 'app-root',
@@ -8,15 +10,45 @@ import { environment } from '../environments/environment'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend'
-  lastBlockNumber: number | undefined
+  wallet: ethers.Wallet | undefined
+  provider: ethers.providers.BaseProvider | undefined
 
-  constructor() {
-    ethers.providers
-      .getDefaultProvider('goerli')
-      .getBlock('latest')
-      .then((block) => {
-        this.lastBlockNumber = block.number
-      })
+  etherBalance = 0
+  tokenBalance = 0
+  votePower = 0
+
+  tokenContract: ethers.Contract | undefined
+
+  constructor() {}
+
+  createWallet() {
+    this.provider = ethers.providers.getDefaultProvider('goerli')
+    this.wallet = ethers.Wallet.createRandom()
+    this.tokenContract = new ethers.Contract(
+      ERC20VOTES_TOKEN_ADDRESS,
+      VoteTokenJson.abi,
+      this.wallet,
+    )
+    this.wallet.getBalance().then((balanceBn) => {
+      this.etherBalance = parseFloat(ethers.utils.formatEther(balanceBn))
+    })
+    this.tokenContract['balanceOf'](this.wallet.address).then(
+      (tokenBalanceBn: BigNumber) => {
+        this.tokenBalance = parseFloat(ethers.utils.formatEther(tokenBalanceBn))
+      },
+    )
+    this.tokenContract['getVotes'](this.wallet.address).then(
+      (votePowerBn: BigNumber) => {
+        this.votePower = parseFloat(ethers.utils.formatEther(votePowerBn))
+      },
+    )
+  }
+
+  vote(voteId: string) {
+    console.log(`Voting for ${voteId}`)
+  }
+
+  request() {
+    console.log(`Requesting token`)
   }
 }
